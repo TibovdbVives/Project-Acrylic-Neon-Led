f385462e5bf2c9c7d9353faae3aea5657b0d8a07
TibovdbVives <152203792+TibovdbVives@users.noreply.github.com>
2023-12-06 16:58:10 +0100

    Add files via upload
    Praktisch finaal arduino code

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,17 +1,30 @@
+#include <Temperature_LM75_Derived.h>
+#include <LiquidCrystal_I2C.h>
 #include <E4SBoard.h>
 E4SClass E4SBoard;
+
+int lcdColumns = 20;
+int lcdRows = 4;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+
+
 
 void setup() 
 {
   // put your setup code here, to run once:
   E4SBoard.Begin();
-  pinMode(2, OUTPUT);
+  
+  lcd.begin();
+  lcd.backlight();
+  
   pinMode(16, OUTPUT);
   pinMode(17, OUTPUT);
-  digitalWrite(2 , LOW);
-  digitalWrite(16, LOW);
+  pinMode(18, OUTPUT);
+  digitalWrite(16 , LOW);
   digitalWrite(17, LOW);
+  digitalWrite(18, LOW);
   Serial.begin(9600);
+  lcd.clear();
 }
 
 void loop() 
@@ -22,21 +35,37 @@ void loop()
     char c = Serial.read();
     if (c == '1')
     {
-      digitalWrite(2, HIGH);
+      lcd.clear();
+      digitalWrite(16, HIGH);
+      lcd.setCursor(0,0);
+      lcd.print("Rood");
+      digitalWrite(17, LOW);
+      digitalWrite(18, LOW);
     }
     if (c == '2')
     {
-      digitalWrite(16, HIGH);
+      lcd.clear();
+      digitalWrite(17, HIGH);
+      lcd.setCursor(0,0);
+      lcd.print("Blauw");
+      digitalWrite(18, LOW);
+      digitalWrite(16, LOW);
     }
     if (c == '3')
     {
-      digitalWrite(17, HIGH);
+      lcd.clear();
+      digitalWrite(18, HIGH);
+      lcd.setCursor(0,0);
+      lcd.print("Groen");
+      digitalWrite(16, LOW);
+      digitalWrite(17, LOW);
     }
     else if (c == '0')
     {
-      digitalWrite(2, LOW);
+      digitalWrite(18, LOW);
       digitalWrite(16, LOW);
       digitalWrite(17, LOW);
+      lcd.clear();
     }
     
   }
